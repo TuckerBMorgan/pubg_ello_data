176e3e51cf946111cdb633af0971e6c094a0bd91
Tucker Morgan <tuckermorgan@Tuckers-MBP.wireless.rit.edu>
2018-02-06 13:30:52 -0500

    "DATE"

--- main.go
+++ main.go
@@ -9,17 +9,18 @@ import (
 )
 
 func generateAllLinks() []string {
+	//strings that are used in the URL for iding each server region
+	//Asia, North America, Korea/Japan, Kakao(seems to be a korean ISP, not sure why it is its own server), South America, Europe, Oceania, South East Asia
 	serverStrings := []string{"as", "na", "krjp", "kakao", "sa", "eu", "oc", "sea"}
-	//queueStrings := []string{"tpp", "fpp"}
 	startString := "https://pubg.op.gg/leaderboard/?server="
-	//  nextString := ""
 	returnStrings := []string{}
 
+	//for each server string region code
 	for _, s := range serverStrings {
+		//add the code to the url to give it the region
 		nextString := startString + s
-		//  fmt.Printf("%v\n", nextString)
-		//returnStrings = append(returnStrings, nextString)
 		for i := 1; i < 3; i++ {
+			//we want the single and duo queue, which is done by adding either 1, or 2 to the end of the url
 			nextString := nextString + "&mode=tpp&queue_size=" + strconv.Itoa(i)
 			returnStrings = append(returnStrings, nextString)
 		}
@@ -28,60 +29,61 @@ func generateAllLinks() []string {
 	return returnStrings
 }
 
-func collectData(urls []string) {
+func collectData(url string) []string {
 
-	for _, url := range urls {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Printf("HEIMDALL Error: There was an error getting %v, %v\n", url, err)
-			continue
-		}
-		parsedPage, err := html.Parse(resp.Body)
-		if err != nil {
-			fmt.Printf("NORN Error: There was an error parsing the page, %v", err)
-			continue
+	scoreStore := []string{}
+	//make a request for the page(leaderboard)
+	resp, err := http.Get(url)
+	if err != nil {
+		//check to makesure that we got some request
+		fmt.Printf("HEIMDALL Error: There was an error getting %v, %v\n", url, err)
+		return []string{}
+	}
+	parsedPage, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Printf("NORN Error: There was an error parsing the page, %v", err)
+		return []string{}
+	}
+	store := scraper(parsedPage)
+	for _, s := range store {
+		scoreStore = append(scoreStore, s)
+	}
+
+	return scoreStore
+}
+
+func scraper(n *html.Node) []string {
+	scoreStore := []string{}
+	//is this an element node
+	if n.Type == html.ElementNode {
+		//for each of the attributes that the element has
+		for _, b := range n.Attr {
+			//if they are the value for the leaderboard rating text
+			if b.Val == "leader-board-top3__rating-value" {
+				//	fmt.Printf("%v\n", n.FirstChild.Data)
+				scoreStore = append(scoreStore, n.FirstChild.Data)
+			}
 		}
 	}
+
+	//traverse the tree of the page
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		returns := scraper(c)
+		for _, s := range returns {
+			scoreStore = append(scoreStore, s)
+		}
+	}
+
+	return scoreStore
 }
 
 func main() {
+	//create all the links for all of the different leaderboard regions, and the for single and duo queue
 	links := generateAllLinks()
-
-	for _, l := range links {
-		fmt.Printf("%v\n", l)
-	}
-
-	return
-	//this makes a request for the asian server, thrid person, singe queue
-	resp, err := http.Get("https://pubg.op.gg/leaderboard/?server=as&mode=tpp&queue_size=1")
-
-	//was there any error any making the request
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-
-	r := resp.Body
-	parsedPage, err := html.Parse(r)
-
-	if err != nil {
-		fmt.Printf("Error %v", err)
-		return
-	}
-
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode {
-			for _, b := range n.Attr {
-				if b.Val == "leader-board-top3__rating-value" {
-					fmt.Printf("%v\n", n.FirstChild.Data)
-				}
-			}
-			//  fmt.Println()
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+	for _, link := range links {
+		scoreStore := collectData(link)
+		for _, score := range scoreStore {
+			fmt.Printf("%v\n", score)
 		}
 	}
-	f(parsedPage)
 }
